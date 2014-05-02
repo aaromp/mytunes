a98f2032d853471dcedd7d1e57de75b7f53b1dd2
Hack Reactor Students <aaromp@gmail.com>
2014-05-01 20:14:01 -0700

    fixes header and styles table with bootstrap classes

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,17 +2,19 @@
 var SongQueueView = Backbone.View.extend({
 
   tagName: "table",
-  // model: "songQueue",
+  className: "table table-hover",
 
   initialize: function() {
-    this.render();
+    this.render(); // initial render
+    // when the collection this is tied to (Songqueue) changes at all,
+    // the view is re-rendered
     this.collection.on('all', function() {
       this.render();
     }, this);
   },
 
   render: function(){
-    this.$el.html('<th>Queue</th>').append(
+    this.$el.html('<th><h1>Queue</h1></th>').append(
       this.collection.map(function(song){
         return new SongQueueEntryView({model: song}).render();
       })
